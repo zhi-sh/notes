4f91c7281426f1bac7a77be1719aa7b6a9854b9e
Z <liuzhi0818@gmail.com>
2020-12-07 23:02:19 +0800

    information retrieval

--- qa_SQuAD/settings.py
+++ qa_SQuAD/settings.py
@@ -2,11 +2,19 @@
 '''
 # @Author       : Zhi
 # @Date         : 2020-12-07 18:12:43
-# @LastEditTime : 2020-12-07 20:44:07
+# @LastEditTime : 2020-12-07 21:50:42
 '''
+import os
 from pathlib import Path
 
 datasets = Path(r'/Users/liuzhi/datasets/SQuAD')
+project_path = os.path.abspath(os.getcwd())
+
+LEMMA_CACHE = f"{project_path}/cache/lemmas.feather"
+VECTOR_CACHE = f"{project_path}/cache/vector.pkl"
+
+BERT_MODEL = '/Users/liuzhi/models/bert-base-uncased'
+
 SQuAD_URL = r'https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json'
 SQuAD_train = datasets / r"train-v2.0.json"
 SQuAD_valid = datasets / r"dev-v2.0.json"
