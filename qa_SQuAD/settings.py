67ea453a043928978f66715d9e74319cd6cfc0db
Z <liuzhi0818@gmail.com>
2020-12-08 22:00:23 +0800

    qa

--- qa_SQuAD/settings.py
+++ qa_SQuAD/settings.py
@@ -2,7 +2,7 @@
 '''
 # @Author       : Zhi
 # @Date         : 2020-12-07 18:12:43
-# @LastEditTime : 2020-12-07 21:50:42
+# @LastEditTime : 2020-12-08 21:23:42
 '''
 import os
 from pathlib import Path
@@ -12,8 +12,11 @@ project_path = os.path.abspath(os.getcwd())
 
 LEMMA_CACHE = f"{project_path}/cache/lemmas.feather"
 VECTOR_CACHE = f"{project_path}/cache/vector.pkl"
+QC_CHCHE = f"{project_path}/cache/question_context.feather"
+DOCBIN_CACHE = f"{project_path}/cache/docs.bin"
+VOCAB_CACHE = f"{project_path}/cache/vocab.bin"
 
-BERT_MODEL = '/Users/liuzhi/models/bert-base-uncased'
+BERT_MODEL = r'/Users/liuzhi/models/torch/bert-large-uncased-whole-word-masking-finetuned-squad'
 
 SQuAD_URL = r'https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json'
 SQuAD_train = datasets / r"train-v2.0.json"
