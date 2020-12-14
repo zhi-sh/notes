cddcc44f49ba769c0a5dc0316a569c8d746d0f04
Z <liuzhi0818@gmail.com>
2020-12-14 21:48:06 +0800

    transformer

--- nlp_classify_THUCnews/datasets.py
+++ nlp_classify_THUCnews/datasets.py
@@ -6,10 +6,13 @@
 import os
 import random
 import codecs
+import pickle
+
 import jieba
 import torch
 from tqdm import tqdm
 from torch.utils.data import Dataset
+from sklearn import preprocessing
 
 from vocab import Vocab
 
@@ -58,8 +61,80 @@ class TxtDataset(Dataset):
         return data, labels, vocab
 
 
+class BertDataset(Dataset):
+    def __init__(self, fpath, tokenizer, maxlen=128, need_label_encoder=True):
+        data, labels = self._read_corpus(fpath)
+        assert len(data) == len(labels)
+        combine_array = list(zip(data, labels))
+        random.shuffle(combine_array)
+        self.data, self.labels = zip(*combine_array)
+
+        # 对label 进行编码
+        if need_label_encoder:
+            root, _ = os.path.split(fpath)
+            self.labels = self._label_encoding(self.labels, os.path.join(root, 'le.pkl'))
+
+        self.tokenizer = tokenizer
+        self.maxlen = maxlen
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, ix):
+        text = self.data[ix]
+        label = self.labels[ix]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.maxlen,
+            return_token_type_ids=True,
+            truncation=True,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
+        return {
+            'text': text,
+            'label': torch.tensor(label, dtype=torch.long),
+            'input_ids': (encoding['input_ids']).flatten(),
+            'attention_mask': (encoding['attention_mask']).flatten(),
+            'token_type_ids': (encoding['token_type_ids']).flatten()
+        }
+
+    def _read_corpus(self, fpath):
+        data = []
+        labels = []
+        with codecs.open(fpath, 'r', encoding='utf-8') as fr:
+            for line in tqdm(fr):
+                line = line.strip()
+                pairs = line.split('\t')
+                if len(pairs) != 2:
+                    print(f"error line : {line}")
+                    continue
+                data.append(pairs[1])
+                labels.append(pairs[0])
+        return data, labels
+
+    def _label_encoding(self, labels, le_path):
+        le = preprocessing.LabelEncoder()
+        label_list = list(set(labels))
+        le.fit(label_list)
+        pickle.dump(le, open(le_path, 'wb'))
+        return le.transform(labels)
+
+
 if __name__ == '__main__':
-    td = TxtDataset(r'/Users/liuzhi/datasets/cnews/cnews.train.txt', maxlen=32, build_vocab=True)
-    for t in td:
-        print(t)
+    # td = TxtDataset(r'/Users/liuzhi/datasets/cnews/cnews.train.txt', maxlen=32, build_vocab=True)
+    # for t in td:
+    #     print(t)
+    #     break
+
+    from transformers import BertTokenizer
+
+    tokenizer = BertTokenizer.from_pretrained(r'/Users/liuzhi/models/torch/bert-base-chinese')
+    bd = BertDataset(r'/Users/liuzhi/datasets/cnews/cnews.val.txt', tokenizer)
+    for d in bd:
+        print(d)
         break
